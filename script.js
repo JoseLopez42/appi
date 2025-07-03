c0f89197b95c366c0bde0d37421aa3a230e3cb23
SALA IBAGUE 109 <salaiba109@cunadm.edu.local>
2025-07-03 15:15:35 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getEmoji() {
+    const res = await fetch("https://api.jikan.moe/v4/seasons/now?sfw")
+    const emoji = await res.json()
+    animeList = ""
+    emoji.data.forEach(anime => {
+        animeList += `<img src="${anime.images.jpg.large_image_url}" alt="">
+                     <h2>${anime.aired.string}</h2>
+                     <h3>${anime.synopsis}</h3>
+                     <button onclick ="saludar('${anime.synopsis}')">Saludar </button>`    
+    });
+    console.log(animeList)
+
+    document.getElementById("emojy").innerHTML = animeList
+
+
+
+    // const html = `<img src="${anime.images.jpg.large_image_url}" alt="">
+    //                 <h2>${anime.aired.string}</h2>
+    //                 <h3>${anime.synopsis}</h3>
+    //                 <button onclick ="saludar('${anime.synopsis}')">Saludar </button>`
+    // document.getElementById("emojy").innerHTML += html
+
+
+}
